cacd9d55e6ac33bf057bfb431c136f0a2e82e01c
Luis Fernando Cezar Dos Santos <nandocezar22@gmail.com>
2023-04-19 20:16:08 -0300

    wip

--- phonebook/src/main/java/br/edu/ifba/phonebook/entities/Contact.java
+++ phonebook/src/main/java/br/edu/ifba/phonebook/entities/Contact.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +24,6 @@ public class Contact {
     private String name;
     private String email;
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "contact_id")
     private List<Number> numbers;
 
     public Contact(String name, String email, List<Number> numbers) {
