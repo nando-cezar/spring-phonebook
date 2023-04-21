993b6566e5510831e6a69949bee62976e2c1121d
Luis Fernando Cezar Dos Santos <nandocezar22@gmail.com>
2023-04-21 16:11:24 -0300

    wip

--- phonebook/src/main/java/br/edu/ifba/phonebook/controller/ContactController.java
+++ phonebook/src/main/java/br/edu/ifba/phonebook/controller/ContactController.java
@@ -18,22 +18,80 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.ifba.phonebook.domain.dto.request.ContactDtoRequest;
 import br.edu.ifba.phonebook.domain.dto.response.ContactDtoResponse;
 import br.edu.ifba.phonebook.service.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(path = "/contacts")
+@Tag(name = "Contacts")
 public class ContactController {
     
     @Autowired
     private ContactService service;
 
     @PostMapping
-    public ResponseEntity<ContactDtoResponse> save(@RequestBody ContactDtoRequest data){
+    @Operation(summary = "Save only one contact")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "201", 
+                description = "Saved with success", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            ),
+            @ApiResponse(
+                responseCode = "406", 
+                description = "Not Acceptable", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            )
+        }
+    )
+    public ResponseEntity<ContactDtoResponse> save(@Parameter(description = "New contact body content to be created") @RequestBody ContactDtoRequest data){
 		var dataDto = service.save(data);
     	return new ResponseEntity<ContactDtoResponse>(dataDto, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<ContactDtoResponse>> find(@RequestParam(required = false) String name){
+    @Operation(summary = "Retrieve all contacts with or without filter")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200", 
+                description = "Retrieval of successful contacts", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            ),
+            @ApiResponse(
+                responseCode = "404", 
+                description = "Not found", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            )
+        }
+    )
+    public ResponseEntity<List<ContactDtoResponse>> find(@Parameter(description = "Name for contact to be found (optional)") @RequestParam(required = false) String name){
         var data = service.find(name).get();
         var isExists = data.isEmpty();
         return isExists ? 
@@ -42,14 +100,64 @@ public class ContactController {
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<ContactDtoResponse> findById(@PathVariable Long id) {
+    @Operation(summary = "Retrieve tip by id")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200", 
+                description = "Retrieval of successful", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            ),
+            @ApiResponse(
+                responseCode = "404", 
+                description = "Not found", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            )
+        }
+    )
+    public ResponseEntity<ContactDtoResponse> findById(@Parameter(description = "Contact Id to be searched") @PathVariable Long id) {
         return service.findById(id)
             .map(record -> ResponseEntity.ok().body(record))
             .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<ContactDtoResponse> update(@PathVariable Long id, @RequestBody ContactDtoRequest data) {
+    @Operation(summary = "Update only one contact")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200", 
+                description = "Updated with successful", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            ),
+            @ApiResponse(
+                responseCode = "404", 
+                description = "Not found", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            )
+        }
+    )
+    public ResponseEntity<ContactDtoResponse> update(@Parameter(description = "Contact Id to be updated") @PathVariable Long id, @Parameter(description = "Contact Elements/Body Content to be updated") @RequestBody ContactDtoRequest data) {
         return service.findById(id)
         .map(record -> {
             var dataSaved = service.update(id, data);
@@ -58,7 +166,32 @@ public class ContactController {
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<ContactDtoResponse> delete(@PathVariable Long id) {
+    @Operation(summary = "Delete only one contact")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200", 
+                description = "Deleted with successful", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            ),
+            @ApiResponse(
+                responseCode = "404", 
+                description = "Not found", 
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ContactDtoResponse.class)
+                    )
+                }    
+            )
+        }
+    )
+    public ResponseEntity<ContactDtoResponse> deleteById(@Parameter(description = "Contact Id to be deleted") @PathVariable Long id) {
         return service.findById(id)
         .map(record -> {
             var data = record;
